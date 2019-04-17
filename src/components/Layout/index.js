36909c9fd86113bd97472cd65195f7c39e7a0aab
Luke Bennett <luke.bennett88@gmail.com>
2019-04-18 09:20:12 +1000

    Save progress

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Header from '../Header';
-import Footer from '../Footer';
+// import Header from '../Header';
+// import Footer from '../Footer';
 
 import '../../css/tailwind.css';
 import '../../css/typography.css';
@@ -10,7 +10,7 @@ import '../../css/global.css';
 const Layout = ({ children }) => (
   <div className="body antialiased flex flex-col leading-relaxed min-h-screen overflow-hidden text-gray-700 w-full">
     {/* <Header /> */}
-    <main className="bg-white flex flex-1 max-w-1920 mx-auto w-full">
+    <main className="bg-white flex flex-1 flex-col max-w-1920 mx-auto w-full">
       {children}
     </main>
     {/* <Footer /> */}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,27 @@
 import React from 'react';
-// import { Link } from 'gatsby';
 
+import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 import Logo from '../images/logo.svg';
 
 const IndexPage = () => (
-  <main className="flex flex-1 flex-col items-center justify-center min-h-screen text-gray-700">
+  <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <img className="h-32 sm:h-64 mb-12" src={Logo} alt="" />
-    <h1 className="text-2xl">Under construction</h1>
-    <p>
-      Website by{' '}
-      <a
-        className="font-bold text-green-500"
-        href="https://phirannodesigns.com.au"
-        rel="nofollow"
-      >
-        Phiranno Designs
-      </a>
-    </p>
-  </main>
+    <div className="flex flex-1 flex-col items-center justify-center">
+      <img className="h-32 sm:h-64 mb-12" src={Logo} alt="" />
+      <h1 className="text-2xl">Under construction</h1>
+      <p>
+        Website by{' '}
+        <a
+          className="font-bold text-green-500"
+          href="https://phirannodesigns.com.au"
+          rel="nofollow"
+        >
+          Phiranno Designs
+        </a>
+      </p>
+    </div>
+  </Layout>
 );
 
 export default IndexPage;
