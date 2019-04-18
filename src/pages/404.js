1f332f4840a4aed30fb9037a409ac47d9b966b4b
Luke Bennett <luke.bennett88@gmail.com>
2019-04-18 11:42:34 +1000

    Update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,20 +3,33 @@ import { Link } from 'gatsby';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
+import Logo from '../images/logo.svg';
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <div className="flex flex-1 flex-col items-center justify-center">
-      <h1 className="text-2xl">404: Page not found</h1>
-      <Link
-        className="bg-green-500 font-semibold inline-block px-4 py-2 text-white"
-        to="/"
-      >
-        Return home
-      </Link>
-    </div>
-  </Layout>
-);
+class NotFoundPage extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+        <div className="flex flex-1 flex-col items-center justify-center">
+          <img className="h-32 sm:h-64 mb-12 mt-auto" src={Logo} alt="" />
+          <h1 className="font-semibold text-2xl">404: Page not found</h1>
+          <Link className="font-bold text-green-500" to="/">
+            Return home
+          </Link>
+          <p className="mt-auto">
+            Website by{' '}
+            <a
+              className="font-bold text-green-500"
+              href="https://phirannodesigns.com.au"
+              rel="nofollow"
+            >
+              Phiranno Designs
+            </a>
+          </p>
+        </div>
+      </Layout>
+    );
+  }
+}
 
 export default NotFoundPage;
