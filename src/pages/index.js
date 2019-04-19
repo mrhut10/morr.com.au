ea9b43cf35e4f1c8da57a5330a6516732f624d8e
Luke Bennett <luke.bennett88@gmail.com>
2019-04-19 13:08:48 +1000

    Style landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,21 +14,21 @@ class IndexPage extends React.Component {
         <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
         <div className="flex flex-1 flex-col items-center justify-center p-4">
           <img className="h-32 sm:h-64 mb-12 mt-auto" src={Logo} alt="" />
-          <h1 className="font-semibold text-2xl">Under construction</h1>
-          <div className="flex flex-wrap">
-            <p className="mr-2">
-              Phone:{' '}
-              <a className="font-bold text-green-500" href={phone}>
-                {phoneFormatted}
-              </a>
-            </p>
-            <p>
-              Email:{' '}
-              <a className="font-bold text-green-500" href={email}>
-                {email}
-              </a>
-            </p>
-          </div>
+          <h1 className="font-light mb-4 text-2xl text-gray-500 tracking-widest uppercase">
+            Under construction
+          </h1>
+          <p>
+            Phone:{' '}
+            <a className="font-bold text-green-500" href={phone}>
+              {phoneFormatted}
+            </a>
+          </p>
+          <p>
+            Email:{' '}
+            <a className="font-bold text-green-500" href={email}>
+              {email}
+            </a>
+          </p>
           <p className="mt-auto">
             Website by{' '}
             <a
