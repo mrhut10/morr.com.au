2f0cd9ceb8ab1551423794513c2f6c425a428ec4
Luke Bennett <hello@lukebennett.com.au>
2019-05-24 16:29:40 +1000

    Update copy and remove unused imports

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { graphql, Link, StaticQuery } from 'gatsby';
+import { graphql, StaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
-import { FaAngleDoubleDown, FaAngleDoubleRight } from 'react-icons/fa';
+import { FaAngleDoubleDown } from 'react-icons/fa';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
+import ContactForm from '../components/Form';
 
 const ContactPage = () => (
   <StaticQuery
@@ -23,7 +24,7 @@ const ContactPage = () => (
         }
         site {
           siteMetadata {
-            title
+            email
           }
         }
       }
@@ -38,7 +39,7 @@ const ContactPage = () => (
           />
           <div className="absolute flex inset-0 items-end justify-center max-w-3xl mx-auto p-4">
             <div className="bg-white leading-none px-2 py-1">
-              <AnchorLink href="#contact">
+              <AnchorLink offset="56" href="#contact">
                 <FaAngleDoubleDown className="text-green-500" />
               </AnchorLink>
             </div>
@@ -51,36 +52,31 @@ const ContactPage = () => (
           <h1 className="font-bold px-4 sm:px-12 lg:px-24 text-green-500 text-4xl uppercase w-full">
             Contact
           </h1>
-          {/* <div className="p-4 sm:px-12 lg:px-24 w-full md:w-1/2">
+          <div className="p-4 sm:px-12 lg:px-24 w-full md:w-1/2">
             <div className="border-green-500 border-l-4 pl-4 text-gray-600 lg:text-xl">
-              <p className="mt-4">
-                <Link
-                  className="flex font-bold items-center text-green-500 uppercase"
-                  to="/projects/black-caviar-pde"
-                >
-                  Black Caviar Pde{' '}
-                  <FaAngleDoubleRight className="text-xs ml-1" />
-                </Link>
+              <p className="mb-4">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum
+                inventore nulla quasi id perferendis modi adipisci excepturi
+                consequuntur magnam, voluptatibus sapiente tempora eius libero
+                autem quas nesciunt odio mollitia eveniet.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque
+                laboriosam quibusdam explicabo est! Hic eum ab laudantium
+                praesentium? Voluptas ab natus illum quod in modi alias qui
+                atque! Neque, deserunt.
+              </p>
+              <p className="font-bold text-gray-700 hover:text-green-500">
+                <a href={`tel:${data.site.siteMetadata.email}`}>
+                  {data.site.siteMetadata.email}
+                </a>
               </p>
             </div>
           </div>
-          <div className="flex justify-center p-4 sm:px-12 lg:text-xl w-full md:w-1/2">
-            <div className="border-4 border-green-500 max-w-sm p-4">
-              <p>
-                <strong>Value:</strong> $58m
-              </p>
-              <p>
-                <strong>Client:</strong> Department of Economic Development,
-                Jobs, Transport and Resources Major Projects Victoria
-              </p>
-              <p>
-                <strong>Architect:</strong> Lovell Chen
-              </p>
-              <p>
-                <strong>Completion Date:</strong> July 2018
-              </p>
-            </div>
-          </div> */}
+          <div className="p-4 sm:px-12 lg:text-xl w-full md:w-1/2">
+            <h2 className="font-bold mb-4 text-gray-800 uppercase">Enquiry</h2>
+            <ContactForm />
+          </div>
         </section>
       </Layout>
     )}
